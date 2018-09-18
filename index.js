a3787e2f00bae8b1feb81e065a69c6bde25d045a
Richard Chen <richard.c.chen@gmail.com>
2018-09-18 10:20:18 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function Scooter(year, color, model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+function Driver(name, age, experience){
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function PickupLocation(address, city){
+  this.address = address
+  this.city = city
+}
+
+
+
+
+
+
+
+
+
+// function User(name, age, hometown) {
+//   this.name = name
+//   this.age = age
+//   this.hometown = hometown
+// }
+//
+// let bobby = new User('bobby', 20, 'Philadelphia')
+// // {name: 'bobby', age: 20, hometown: 'Philadelphia'}
